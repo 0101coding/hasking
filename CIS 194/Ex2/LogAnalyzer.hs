495edb55cf1dc99532a2a0fab2fd370240502354
mayowa <mayowa@dipolediamond.com>
2019-11-01 00:03:47 +0100

    fixed issue with ParseMessage

--- CIS 194/Ex2/LogAnalyzer.hs
+++ CIS 194/Ex2/LogAnalyzer.hs
@@ -15,8 +15,8 @@ parseMessage c
               first = head $ arr
               second = head $ tail arr
               third = head $ tail $ tail arr
-              fromThird = unwords $  drop 4 arr
-              fromFourth = unwords $ drop 4 arr
+              fromThird = unwords $  drop 2 arr
+              fromFourth = unwords $ drop 3 arr
 
 insert :: LogMessage  -> MessageTree -> MessageTree
 insert lm Leaf = Node Leaf lm Leaf
@@ -26,3 +26,10 @@ insert l1@(LogMessage _ x _) m@(Node left l2@(LogMessage _ y _) right)
         | x > y = Node left l2 (insert l1 right) 
         | otherwise = m
 insert _ m@(Node _ (Unknown _) _) = m
+
+
+build ::[LogMessage] -> MessageTree
+build = undefined
+
+mTree :: MessageTree
+mTree = Node Leaf (parseMessage "I 29 la la la") Leaf
